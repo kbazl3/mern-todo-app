79986584aec854c5497f3b25a680c46082ac6136
Kyle Barlow <kylebarlow@Kyles-MBP.weberpl.lib.ut.us>
2019-06-10 18:31:47 -0600

    setting up edit component

--- src/components/edit-todo.component.js
+++ src/components/edit-todo.component.js
@@ -1,6 +1,20 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class EditTodo extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        }
+    }
+
+    
+
     render() {
         return(
             <div>
--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,10 +1,61 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+import axios from 'axios';
+
+
+const Todo = props => (
+    <tr> 
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_description}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_responsible}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
+        <td>
+            <Link to={'/edit/'+props.todo._id}>Edit</Link>
+        </td>
+    </tr>
+)
+
 
 export default class TodosList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {todos: []}
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/todos/')
+            .then(response => {
+                this.setState({todos: response.data})
+                console.log(this.state)
+            })
+            .catch(function(err) {
+                console.log(err)
+            })
+    }
+
+    todolist() {
+        return this.state.todos.map((currentTodo, index) => {
+            return <Todo todo={currentTodo} key={index} />;
+        })
+    }
+
     render() {
         return(
             <div>
-                <p>TodosList</p>
+                <h3>Todos List</h3>
+                <table className="table table-striped" style={{marginTop: 20}}>
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Responsible</th>
+                            <th>Priority</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.todolist()}
+                    </tbody>
+                </table>
             </div>
         )
     }
